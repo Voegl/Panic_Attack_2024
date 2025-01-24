283d1162dde1d0c0405d578191623f7bdaa45943
nerdyraspberry <107356356+nerdyraspberry@users.noreply.github.com>
2025-01-24 18:32:31 +0100

    Kowalski's derde analyse

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KOPwalski.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KOPwalski.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.GuesstimatesProvider;
 import org.firstinspires.ftc.teamcode.subsystems.Slides;
 
 @TeleOp(name = "kOPwalski")
@@ -26,6 +27,53 @@ public class KOPwalski extends OpMode {
 
     @Override
     public void loop() {
-        this.drivetrain.setDriveSpeeds(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.left_trigger - gamepad1.right_trigger);
+        // Controller 1
+        // - Driving
+        drivetrain.setDriveSpeeds(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.left_trigger - gamepad1.right_trigger);
+
+        // - Container
+        if (gamepad1.a) // Drop sample from container
+            slides.setAngle(GuesstimatesProvider.CONTAINER_DROP_ROTATION);
+        else if (gamepad1.b) // Return container to holding position
+            slides.setAngle(GuesstimatesProvider.CONTAINER_HOLD_ROTATION);
+
+        // Controller 2
+        // - Slides
+        if (gamepad2.dpad_down) // Initial position
+            slides.setPosition(0, GuesstimatesProvider.SLIDES_SPEED);
+        else if (gamepad2.dpad_left) // Lower basket
+            slides.setPosition(GuesstimatesProvider.LOWER_BASKET_POSITION, GuesstimatesProvider.SLIDES_SPEED);
+        else if (gamepad2.dpad_up) // Upper basket
+            slides.setPosition(GuesstimatesProvider.UPPER_BASKET_POSITION, GuesstimatesProvider.SLIDES_SPEED);
+
+        // - Arm
+        // - - Movement
+        if (gamepad2.x) // Arm to container
+            arm.smartyPantsSetPosition(
+                GuesstimatesProvider.ARM_CONTAINER_POSITION,
+                GuesstimatesProvider.ARM_SPEED,
+                GuesstimatesProvider.ANGLER_SERVO_CONTAINER_POSITION
+            );
+        else if (gamepad2.y) // Arm to gripping position
+            arm.smartyPantsSetPosition(
+                GuesstimatesProvider.ARM_GRIPPING_POSITION,
+                GuesstimatesProvider.ARM_SPEED,
+                GuesstimatesProvider.ANGLER_SERVO_GRIPPING_POSITION
+            );
+
+        // Manual control
+        double manualDirection = gamepad2.left_trigger - gamepad2.right_trigger; // This might need inversion
+        if (manualDirection != 0) // This shouldn't always run, that would screw with the automatic positioning
+            arm.rotateArm(manualDirection * GuesstimatesProvider.ARM_SPEED);
+
+        // - - Rotation
+        // for now automatic, see arm.smartyPantsSetPosition
+
+        // - - Gripping
+        if (gamepad2.a) // Open gripper
+            arm.setGripperAngle(GuesstimatesProvider.GRIPPER_SERVO_OPEN_ROTATION);
+        else if (gamepad2.b) // Close gripper
+            arm.setGripperAngle(GuesstimatesProvider.GRIPPER_SERVO_CLOSE_ROTATION);
+
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/GuesstimatesProvider.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+// copy template:
+// public final static
+
+public class GuesstimatesProvider {
+    public final static double CONTAINER_DROP_ROTATION = 0d;
+    public final static double CONTAINER_HOLD_ROTATION = 1d;
+
+    public final static double SLIDES_SPEED = 1d;
+
+    public final static int LOWER_BASKET_POSITION = 10;
+    public final static int UPPER_BASKET_POSITION = 15;
+
+    public final static double ARM_SPEED = 1d;
+    public final static int ARM_CONTAINER_POSITION = 0;
+    public final static int ARM_GRIPPING_POSITION = 10;
+
+    public final static double ANGLER_SERVO_CONTAINER_POSITION = 0d;
+    public final static double ANGLER_SERVO_GRIPPING_POSITION = 1d;
+
+    public final static double GRIPPER_SERVO_OPEN_ROTATION = 0d;
+    public final static double GRIPPER_SERVO_CLOSE_ROTATION = 1d;
+}
